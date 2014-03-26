74c672570ddde2ff4f077b0f83c2de8157ff34bd
Weston Hanners <weston@hanners.us>
2014-03-25 21:05:20 -0400

    Initial Commit

---
+++ Tutorial 1/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  Tutorial 1
+//
+//  Created by Weston Hanners on 3/25/14.
+//  Copyright (c) 2014 Hanners Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tutorial 1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tutorial 1
+//
+//  Created by Weston Hanners on 3/25/14.
+//  Copyright (c) 2014 Hanners Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}
